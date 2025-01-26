73a05cb837da5c7f3d50323a1b78bca498b41c6f
pip64 <76626915+pip64@users.noreply.github.com>
2025-01-26 19:46:48 +0700

    added comments

--- lang/lexer.js
+++ lang/lexer.js
@@ -24,6 +24,11 @@ function lex(input) {
             continue;
         }
 
+        if ((match = input.slice(pos).match(/^#.*/))) {
+            pos += match[0].length;
+            continue;
+        }
+
         if ((match = input.slice(pos).match(/^(yeezy|bleached|change|spit|repeat|times|be|to)\b/))) {
             tokens.push({ type: 'keyword', value: match[0] });
             pos += match[0].length;
