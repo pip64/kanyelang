c99541cf3515a1c71bbbe868590114c870a085be
pip64 <76626915+pip64@users.noreply.github.com>
2025-01-26 19:48:54 +0700

    Changed config and new build

--- cli/config.js
+++ cli/config.js
@@ -1,9 +1,4 @@
 module.exports = {
     name: "KanyeLang",
-    version: "1.0.0",
-    features: [
-        "🔥 Strict Mode",
-        "🎤 Advanced CLI",
-        "🚀 Eval Execution"
-    ]
+    version: "1.0.0"
 };
